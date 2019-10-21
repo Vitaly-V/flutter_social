6f9880974a109cd52679a9dba9d7ac83049374a3
Vitaly-V <gtopbox@gmail.com>
2019-10-21 17:45:46 +0300

    Add reusable header

--- lib/widgets/header.dart
+++ lib/widgets/header.dart
@@ -1,5 +1,16 @@
 import 'package:flutter/material.dart';
 
-header() {
-  return Text("header");
+header(BuildContext context, {bool isAppTitle = false, String titleText = ''}) {
+  return AppBar(
+    title: Text(
+      isAppTitle ? 'Flutter Social' : titleText,
+      style: TextStyle(
+        color: Colors.white,
+        fontFamily: isAppTitle ? 'Signatra' : '',
+        fontSize: isAppTitle ? 50 : 22,
+      ),
+    ),
+    centerTitle: true,
+    backgroundColor: Theme.of(context).accentColor,
+  );
 }
